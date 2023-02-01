9d0c135bddacbcc2a9b714b5016b22b4718e95ee
Naglis Timofejevas <timofejevasnaglis@gmail.com>
2023-02-01 13:58:53 +0200

    Aprasytas admin

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here
+from .models import Author, Genre, Book, BookInstace
+
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(BookInstace)
--- mysite/library/models.py
+++ mysite/library/models.py
@@ -5,16 +5,23 @@ import uuid
 class Genre(models.Model):
     name = models.CharField(verbose_name="Pavadinimas", max_length=200, help_text="Iveskite knygos zanra (pvz. detektyvas")
 
+    def __str__(self):
+        return f"{self.name}"
 class Book(models.Model):
     title = models.CharField(verbose_name="Pavadinimas", max_length=200)
     summary = models.TextField(verbose_name="Aprasymas", max_length=1000)
     isbn = models.CharField(verbose_name="ISBN", max_length=13, help_text="ISBN unikalus knygos kodas")
     author = models.ForeignKey(to="Author", on_delete=models.SET_NULL, null=True)
     genre  = models.ManyToManyField(to="Genre")
+
+    def __str__(self):
+        return f"{self.title}"
 class Author(models.Model):
     first_name = models.CharField(verbose_name="Vardas", max_length=100)
     last_name = models.CharField(verbose_name="Pavarde", max_length=100)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 class BookInstace(models.Model):
     book = models.ForeignKey(to="Book", on_delete=models.CASCADE)
     uuid = models.UUIDField(verbose_name="UUID", default=uuid.uuid4, help_text="Unikalus ID knygos raktas")
@@ -34,3 +41,5 @@ class BookInstace(models.Model):
         default="a",
         help_text="Statusas",
     )
+    def __str__(self):
+        return f"{self.book} - {self.uuid}"
