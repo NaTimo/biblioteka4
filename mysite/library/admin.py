19f76d42162343db8c6cd73d8256fa6c4a593218
Naglis Timofejevas <timofejevasnaglis@gmail.com>
2023-02-02 15:01:01 +0200

    Prideta BookIntance redagavimas

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 # Register your models here
 from .models import Author, Genre, Book, BookInstance
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "display_books"]
 class BookInstanceInLine(admin.TabularInline):
     model = BookInstance
+    readonly_fields = ("uuid", )
     extra = 0
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "isbn", "display_genre")
@@ -12,13 +16,15 @@ class BookAdmin(admin.ModelAdmin):
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ("book", "status", "due_back")
     list_filter = ("status", "due_back")
+    search_fields = ("uuid", "book__title")
+    list_editable = ("due_back", "status")
 
     fieldsets = (
         ("General", {"fields": ("uuid", "book")}),
         ("Availability", {"fields": ("status", "due_back")}),
     )
 
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookInstance, BookInstanceAdmin)
